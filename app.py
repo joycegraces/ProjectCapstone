c47c86cb2434eddb37ff6b366656f63303b907af
joycegraces <joyce.grace@ti.ukdw.ac.id>
2022-06-04 19:37:18 +0700

    commit2

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from tkinter import *
+from posixpath import split
+from tes import get_respond
 
-
-BG_GRAY = "#ABB2B9"
-BG_COLOR = "#17202A"
+BG_GRAY = "#827397"
+BG_COLOR = "#4C3575"
 TEXT_COLOR = "#EAECEE"
 
-FONT = "Helvetica 14"
+FONT = "Helvetica 12"
 FONT_BOLD = "Helvetica 13 bold"
 
 class ChatApplication:
@@ -18,18 +19,19 @@ class ChatApplication:
         self.window.mainloop()
         
     def _setup_main_window(self):
-        self.window.title("Chat")
+        self.window.title("Gacoan Chatbot")
         self.window.resizable(width=False, height=False)
+        self.window.geometry("550x550")
         self.window.configure(width=470, height=550, bg=BG_COLOR)
         
         # head label
         head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
-                           text="Welcome", font=FONT_BOLD, pady=10)
+                           text="Welcome to Gacoan Chatbot", font=FONT_BOLD, pady=10)
         head_label.place(relwidth=1)
         
         # tiny divider
         line = Label(self.window, width=450, bg=BG_GRAY)
-        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        line.place(relwidth=1, rely=0.5, relheight=0.012)
         
         # text widget
         self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
@@ -47,31 +49,33 @@ class ChatApplication:
         bottom_label.place(relwidth=1, rely=0.825)
         
         # message entry box
-        self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
-        self.msg_entry.place(relwidth=0.79, relheight=0.03, rely=0.030, relx=0.015)
+        self.msg_entry = Entry(bottom_label, bg="#4C3575", fg=TEXT_COLOR, font=FONT)
+        self.msg_entry.place(relwidth=0.79, relheight=0.0325, rely=0.025, relx=0.015)
         self.msg_entry.focus()
         self.msg_entry.bind("<Return>", self._on_enter_pressed)
         
         # send button
-        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
+        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_COLOR,
                              command=lambda: self._on_enter_pressed(None))
-        send_button.place(relx=0.83, rely=0.030, relheight=0.03, relwidth=0.15)
+        send_button.place(relx=0.83, rely=0.025, relheight=0.03, relwidth=0.15)
      
     def _on_enter_pressed(self, event):
         msg = self.msg_entry.get()
-        self._insert_message(msg, "You")
+        self._insert_message(msg, "Pelanggan")
         
     def _insert_message(self, msg, sender):
         if not msg:
             return
         
         self.msg_entry.delete(0, END)
-        msg1 = f"{sender}: {msg}\n\n"
+        msg1 = f"Pelanggan\t> {msg}\n\n"
         self.text_widget.configure(state=NORMAL)
         self.text_widget.insert(END, msg1)
         self.text_widget.configure(state=DISABLED)
         
-        msg2 = f"{bot_name}: {get_response(msg)}\n\n"
+
+
+        msg2 = f"GacoanMin\t> {get_respond(msg)}\n\n\n"
         self.text_widget.configure(state=NORMAL)
         self.text_widget.insert(END, msg2)
         self.text_widget.configure(state=DISABLED)
--- tes.py
+++ tes.py
@@ -1,13 +1,17 @@
-import aiml
+import aiml, os
 # membuat kernel dan mempelajari berkas AIML
-kernel = aiml.Kernel()
-kernel.learn("utama.xml")
-kernel.respond("aiml a")
-while True:
-    #print(kernel.respond(input("User: >> ")))
 
-    reply = kernel.respond(input("User > "))
-    if reply:
-        print("bot > ", reply)
+kern = aiml.Kernel()
+if os.path.isfile("bot_brain.brn"):
+    kern.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kern.bootstrap(learnFiles = "star.xml", commands = "AIML A")
+    kern.saveBrain("bot_brain.brn")
+
+
+def get_respond(msg):
+    #print(kernel.respond(input("User: >> ")))
+    if (kern.respond(msg)):
+        return (kern.respond(msg))
     else:
-        print("bot >stop", )
+        return  "Maaf Ka, saya kurang mengerti. Bisakah diulangi lagi?"
